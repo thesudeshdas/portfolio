dc7f187a324774100b1776cb3d9002e54d2d3e53
thesudeshdas <sudeshkumardas7@gmail.com>
2025-08-13 10:57:42 +0530

    chore: add prettier-plugin-tailwindcss and update dev script port

--- components/AppNav/appNav.data.ts
+++ components/AppNav/appNav.data.ts
@@ -4,8 +4,8 @@ import { IAppNavLink } from '@/types/navs/navs.types';
 export const appNavLinks: IAppNavLink[] = [
   {
     id: 'a97541c3-b5b4-4055-8558-a41b732d07b0',
-    link: '/dev',
-    text: 'Dev'
+    link: '/code',
+    text: 'Code'
   },
   // {
   //   id: uuidv4(),
@@ -14,8 +14,8 @@ export const appNavLinks: IAppNavLink[] = [
   // },
   {
     id: 'e25e872f-42c4-4e7e-835c-28626be3c1bd',
-    link: '/daisy',
-    text: 'Daisy'
+    link: '/stories',
+    text: 'Stories'
   },
   {
     id: 'b8762c4d-e3f5-4a66-9c7d-2e89b3f51a90',
